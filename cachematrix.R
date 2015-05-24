b212f963f3d2255754e200bb0d2164d17240411a
saratechnology <sara.ltecnologia@gmail.com>
2015-05-24 11:34:51 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,20 @@
 ##create a function which starts with a null matrix argument
 makeCacheMatrix <- function(x = matrix()) { 
   ## initialize the value of the matrix inverse to NULL
-  matrixinverse <- NULL                     
-  ## delcare another function set where the value will be cached in 1. Matrix is created
+  mtxinv <- NULL                     
+  ## declare another function set where the value will be cached in 1. Matrix is created
   ## for the first time. 2. changes made to cached matrix
   set <- function(y) {                      
     x <<- y
     ## change the value of inverse of the matrix in case the matrix was changed.
-    matrixinverse <<- NULL              
+    mtxinv <<- NULL              
   }
   ## gets the value of the inverse
   get <- function() x                           
   #calculates the inverse of non-singular matrix via the solve function
-  setinverse <- function(solve) matrixinverse <<- solve 
+  setinverse <- function(solve) mtxinv <<- solve 
   # gets the inverse     
-  getinverse <- function() matrixinverse        
+  getinverse <- function() mtxinv        
   ## passes the value of the function makeCacheMatrix        
   list(set = set, get = get,                    
        setinverse = setinverse,
@@ -23,15 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 # used to get the cache of the matrix
 cacheSolve<- function(x, ...) {                 
-  matrixinverse <- x$getinverse()
+  mtxinv <- x$getinverse()
   #if the inverse exists, it gets it.
-  if(!is.null(matrixinverse)) {                 
+  if(!is.null(mtxinv)) {                 
     message("getting cached data - Inverse of the matrix")
-    return(matrixinverse)
+    return(mtxinv)
   }
   #if the inverse if not there, first it is calculated and then retrieved.
   data <- x$get()                               
-  matrixinverse <- solve(data, ...)
-  x$setinverse(matrixinverse)
-  matrixinverse
+  mtxinv <- solve(data, ...)
+  x$setinverse(mtxinv)
+  mtxinv
 }
